558b83480274c020fd243976af8d787f9fef993e
Valerik <valerikr1997@mail.ru>
2020-02-19 17:18:36 +0500

    Медиа и переделка слайдера + мелочи

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-    $('.main__section_sl').slick();
+    $('.main__section_sl').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          
+        ]
+      });
   });
